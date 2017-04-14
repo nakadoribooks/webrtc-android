8d3979d996596b319cc71881102a592f4f1214e4
yu kawase <kawase@aloe-project.com>
2017-04-14 23:09:52 +0900

    only WebRTC

--- app/src/main/java/com/nakadoribooks/webrtcexample/WebRTC.java
+++ app/src/main/java/com/nakadoribooks/webrtcexample/WebRTC.java
@@ -3,12 +3,10 @@ package com.nakadoribooks.webrtcexample;
 import android.app.Activity;
 import android.content.Context;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.WindowManager;
 import org.webrtc.Camera2Enumerator;
 import org.webrtc.CameraEnumerator;
 import org.webrtc.EglBase;
-import org.webrtc.Logging;
 import org.webrtc.MediaStream;
 import org.webrtc.PeerConnectionFactory;
 import org.webrtc.RendererCommon;
@@ -23,8 +21,6 @@ import org.webrtc.VideoTrack;
  */
 
 public class WebRTC {
-
-    private static final String TAG = "WebRTC";
 
     private PeerConnectionFactory factory;
     private VideoCapturer videoCapturer;
@@ -58,11 +54,7 @@ public class WebRTC {
 
     private void setupLocalStream() {
 
-        Log.d(TAG, "setupLocalStream 1");
-
         SurfaceViewRenderer localRenderer = setupRenderer();
-
-        Log.d(TAG, "setupLocalStream 3");
 
         MediaStream localStream = factory.createLocalMediaStream("android_local_stream");
         videoCapturer = createCameraCapturer(new Camera2Enumerator(activity));
@@ -83,12 +75,9 @@ public class WebRTC {
         windowManager.getDefaultDisplay().getRealMetrics(displayMetrics);
         int videoWidth = displayMetrics.widthPixels;
         int videoHeight = displayMetrics.heightPixels;
-        Logging.d(TAG, "videoWidth:" + videoWidth + ", height:" + videoHeight);
 
         videoCapturer.startCapture(videoWidth, videoHeight, 30);
     }
-
-
 
     private SurfaceViewRenderer setupRenderer(){
         SurfaceViewRenderer localRenderer = (SurfaceViewRenderer) activity.findViewById(R.id.local_render_view);
@@ -107,34 +96,11 @@ public class WebRTC {
     private VideoCapturer createBackCameraCapturer(CameraEnumerator enumerator) {
         final String[] deviceNames = enumerator.getDeviceNames();
 
-        Logging.d(TAG, "Looking for other cameras.");
         for (String deviceName : deviceNames) {
             if (!enumerator.isFrontFacing(deviceName)) {
-                Logging.d(TAG, "Creating other camera capturer.");
-                Logging.d(TAG, "DeviceName: " + deviceName);
                 VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
 
                 if (videoCapturer != null) {
-                    Logging.d(TAG, "foundVideoCapture: " + videoCapturer.toString());
-                    return videoCapturer;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private VideoCapturer createFrontCameraCapturer(CameraEnumerator enumerator) {
-        final String[] deviceNames = enumerator.getDeviceNames();
-
-        Logging.d(TAG, "Looking for front facing cameras.");
-        for (String deviceName : deviceNames) {
-            if (enumerator.isFrontFacing(deviceName)) {
-                Logging.d(TAG, "Creating front facing camera capturer.");
-                Logging.d(TAG, "DeviceName: " + deviceName);
-                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
-                if (videoCapturer != null) {
-                    Logging.d(TAG, "foundVideoCapture: " + videoCapturer.toString());
                     return videoCapturer;
                 }
             }
